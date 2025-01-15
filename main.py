520b87dcf161d03fc44d6b2769b0c360975876cd
Franco Novoa <franconovoa3@gmail.com>
2025-01-15 18:33:19 -0500

    Add Person class

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import csv
 
+class Person:
+    def __init__(self, age, sex, bmi, children, smoker, region, charges):
+        self.age = age
+        self.sex = sex
+        self.bmi = bmi
+        self.children = children
+        if smoker == "yes":
+            self.smoker = True
+        else:
+            self.smoker = False
+        self.region = region
+        self.charges = charges
+
+
 def main():
     people = []
     with open("insurance.csv", mode="r", newline="") as file:
